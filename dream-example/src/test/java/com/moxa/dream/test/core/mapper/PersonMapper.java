27103b734f7e2425ef619e6d38dfbf6738b5de6a
moxa <1339596978@qq.com>
2022-03-20 22:29:22 +0800

    update

--- dream-example/src/test/java/com/moxa/dream/test/core/mapper/PersonMapper.java
+++
@@ -1,5 +0,0 @@
-package com.moxa.dream.test.core.mapper;
-
-public interface PersonMapper {
-    int test(int id);
-}
--- dream-example/src/test/java/com/moxa/dream/test/core/view/MyView.java
+++
@@ -1,7 +0,0 @@
-package com.moxa.dream.test.core.view;
-
-
-public class MyView {
-    private int id;
-    private MyViewUser myViewUser;
-}
