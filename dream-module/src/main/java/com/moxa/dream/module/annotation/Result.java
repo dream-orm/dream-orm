bebc97de023200c5eda91fb0aadb57103d6be4e0
moxa <1339596978@qq.com>
2022-03-21 12:36:43 +0800

    update

--- dream-module/src/main/java/com/moxa/dream/module/annotation/Result.java
+++ dream-module/src/main/java/com/moxa/dream/module/annotation/Result.java
@@ -2,8 +2,14 @@ package com.moxa.dream.module.annotation;
 
 import com.moxa.dream.module.reflect.util.NullObject;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Collection;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Result {
     Class<? extends Collection> rowType() default NullObject.class;
 
