d30d13108607f5ef04a1f56c9968baf54a99bacf
moxa <1339596978@qq.com>
2022-12-30 17:24:22 +0800

    update

--- dream-template/src/main/java/com/moxa/dream/template/service/ServiceImpl.java
+++ dream-template/src/main/java/com/moxa/dream/template/service/ServiceImpl.java
@@ -8,7 +8,7 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
 
-public class ServiceImpl<ListView, EditView> implements IService<ListView, EditView> {
+public abstract class ServiceImpl<ListView, EditView> implements IService<ListView, EditView> {
     protected TemplateMapper templateMapper;
     private Class listViewType;
     private Class editViewType;
