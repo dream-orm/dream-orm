40b98eb34539e651daf6eee67d107644023535f2
moxa <1339596978@qq.com>
2023-01-06 22:53:42 +0800

    update

--- dream-system/src/main/java/com/moxa/dream/system/core/action/SqlAction.java
+++ dream-system/src/main/java/com/moxa/dream/system/core/action/SqlAction.java
@@ -5,33 +5,19 @@ import com.moxa.dream.system.config.Configuration;
 import com.moxa.dream.system.config.MappedStatement;
 import com.moxa.dream.system.config.MethodInfo;
 import com.moxa.dream.system.core.session.Session;
-import com.moxa.dream.util.common.NonCollection;
 import com.moxa.dream.util.common.ObjectMap;
 import com.moxa.dream.util.common.ObjectUtil;
-import com.moxa.dream.util.common.ObjectWrapper;
 import com.moxa.dream.util.exception.DreamRunTimeException;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class SqlAction implements Action {
-    private String property;
-
     private MethodInfo methodInfo;
+    private BiConsumer<Object, Object> consumer;
 
-    public SqlAction(Configuration configuration, String sql) {
-        this(configuration, null, sql);
-    }
-
-    public SqlAction(Configuration configuration, String property, String sql) {
-        this(configuration, property, sql, NonCollection.class, Object.class);
-    }
-
-    public SqlAction(Configuration configuration, String property, String sql, Class<? extends Collection> rowType, Class<?> colType) {
-        this(configuration, property, sql, rowType, colType, true);
-    }
-
-    public SqlAction(Configuration configuration, String property, String sql, Class<? extends Collection> rowType, Class<?> colType, boolean cache) {
+    public SqlAction(Configuration configuration, String sql, Class<? extends Collection> rowType, Class<?> colType, BiConsumer<Object, Object> consumer) {
         if (configuration == null) {
             throw new DreamRunTimeException("configuration不能为空");
         }
@@ -44,27 +30,26 @@ public class SqlAction implements Action {
         if (colType == null) {
             throw new DreamRunTimeException("colType不能为空");
         }
-        this.property = property;
+        if (consumer == null) {
+            throw new DreamRunTimeException("consumer不能为空");
+        }
         methodInfo = new MethodInfo()
                 .setConfiguration(configuration)
                 .setRowType(rowType)
                 .setColType(colType)
-                .setSql(sql)
-                .setCache(cache);
+                .setSql(sql);
+        this.consumer = consumer;
     }
 
     @Override
-    public void doAction(Session session, MappedStatement mappedStatement, Object arg) throws Exception {
+    public void doAction(Session session, MappedStatement mappedStatement, Object arg) {
         Map<String, Object> argMap;
         if (arg instanceof Map) {
             argMap = (Map<String, Object>) arg;
         } else {
             argMap = new ObjectMap(arg);
         }
-        ObjectWrapper wrapper = ObjectWrapper.wrapper(arg);
         Object result = session.execute(methodInfo, argMap);
-        if (!ObjectUtil.isNull(property)) {
-            wrapper.set(property, result);
-        }
+        consumer.accept(arg, result);
     }
 }
