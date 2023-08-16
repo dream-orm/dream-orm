1578ea2e9e069a123b810a8f945cf7cee5579777
moxa <1339596978@qq.com>
2023-08-16 21:42:19 +0800

    增加测试案列

--- dream-orm-chain/src/main/java/com/dream/chain/mapper/FlexChainMapper.java
+++ dream-orm-chain/src/main/java/com/dream/chain/mapper/FlexChainMapper.java
@@ -7,12 +7,35 @@ import com.dream.chain.def.ChainUpdateColumnDef;
 import com.dream.flex.def.ColumnDef;
 import com.dream.flex.def.TableDef;
 
+/**
+ * 链式强化接口
+ */
 public interface FlexChainMapper {
+    /**
+     * 查询链式操作
+     * @param columnDefs 查询字段集
+     * @return 查询链式
+     */
     ChainSelectDef select(ColumnDef... columnDefs);
 
+    /**
+     * 更新链式操作
+     * @param tableDef 表
+     * @return 更新链式
+     */
     ChainUpdateColumnDef update(TableDef tableDef);
 
+    /**
+     * 插入链式操作
+     * @param tableDef 表
+     * @return 插入链式
+     */
     ChainInsertIntoTableDef insertInto(TableDef tableDef);
 
+    /**
+     * 删除链式操作
+     * @param tableDef 表
+     * @return 删除链式
+     */
     ChainDeleteTableDef delete(TableDef tableDef);
 }
--- dream-orm-test/dream-orm-hello-world/src/test/java/com/dream/helloworld/HelloWorldFlexChainTest.java
+++ dream-orm-test/dream-orm-hello-world/src/test/java/com/dream/helloworld/HelloWorldFlexChainTest.java
@@ -1,13 +1,22 @@
 package com.dream.helloworld;
 
 import com.dream.chain.mapper.FlexChainMapper;
+import com.dream.flex.def.Delete;
+import com.dream.flex.def.Insert;
+import com.dream.flex.def.Query;
+import com.dream.flex.def.Update;
 import com.dream.helloworld.view.AccountView;
+import com.dream.system.config.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static com.dream.flex.def.FunctionDef.*;
+import static com.dream.flex.def.FunctionDef.delete;
 import static com.dream.helloworld.table.table.AccountTableDef.account;
 
 @RunWith(SpringRunner.class)
@@ -28,4 +37,51 @@ public class HelloWorldFlexChainTest {
                 .one(AccountView.class);
         System.out.println("查询结果：" + accountView);
     }
+
+    /**
+     * 测试查询多条
+     */
+    @Test
+    public void testSelectList() {
+        List<AccountView> accountViews = flexChainMapper.select(account.accountView).from(account)
+                .where(account.id.gt(3)).list(AccountView.class);
+        System.out.println("查询结果：" + accountViews);
+    }
+
+    /**
+     * 测试分页查询
+     */
+    @Test
+    public void testSelectPage() {
+        Page page=new Page(1,2);
+        page=flexChainMapper.select(account.accountView).from(account).where(account.id.gt(1)).page(AccountView.class,page);
+        System.out.println("总数："+page.getTotal()+"\n查询结果：" + page.getRows());
+    }
+
+    /**
+     * 测试更新
+     */
+    @Test
+    public void testUpdate() {
+        flexChainMapper.update(account).set(account.age, account.age.add(1))
+                .set(account.name, "accountName")
+                .where(account.id.eq(1)).execute();
+    }
+
+    /**
+     * 测试插入
+     */
+    @Test
+    public void testInsert() {
+        flexChainMapper.insertInto(account).columns(account.name, account.age).values("accountName", 12).execute();
+    }
+
+    /**
+     * 测试删除
+     */
+    @Test
+    public void testDelete() {
+        flexChainMapper.delete(account).where(account.id.eq(1)).execute();
+    }
+
 }
