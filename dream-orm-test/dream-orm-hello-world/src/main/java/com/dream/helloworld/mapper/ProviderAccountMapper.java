22f011d4a1cf3d20d1c9cb582fb20f612fb3e911
moxa <1339596978@qq.com>
2023-08-16 23:50:27 +0800

    增加测试案列

---
+++ dream-orm-test/dream-orm-hello-world/src/main/java/com/dream/helloworld/mapper/ProviderAccountMapper.java
@@ -0,0 +1,16 @@
+package com.dream.helloworld.mapper;
+
+import com.dream.helloworld.mapper.provider.AccountProvider;
+import com.dream.helloworld.view.AccountView;
+import com.dream.system.annotation.Mapper;
+import com.dream.system.annotation.Param;
+
+import java.util.List;
+
+@Mapper(AccountProvider.class)
+public interface ProviderAccountMapper {
+   AccountView selectProvideById(@Param("id") int id);
+
+   List<AccountView> selectProvideList(@Param("account") AccountView accountView);
+
+}
