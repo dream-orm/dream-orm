b90848cf1be5e931eafea921455183fa1328de46
moxa <1339596978@qq.com>
2023-08-21 23:01:46 +0800

    tdengine测试

--- dream-orm-hello-world-tdengine/src/test/java/com/dream/tdhelloworld/HelloWorldTdTest.java
+++ dream-orm-hello-world-tdengine/src/test/java/com/dream/tdhelloworld/HelloWorldTdTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -57,5 +59,29 @@ public class HelloWorldTdTest {
     public void testSession() throws Exception {
         List<Map> list = flexTdChainMapper.select(col("voltage"),first("ts")).from("meters").session("ts","10s").limit(1, 10).list(Map.class);
         System.out.println("查询结果：" + list);
+    }
+    /**
+     * 测试插入
+     */
+    @Test
+    public void testInsert(){
+        flexTdChainMapper.insertInto("d1001").values(new Date(),10.2,219,0.32).execute();
+    }
+    /**
+     * 测试插入多条
+     */
+    @Test
+    public void testInsertMany(){
+        List<Object[]>list=new ArrayList<>();
+        list.add(new Object[]{new Date(),10.2,219,0.32});
+        list.add(new Object[]{new Date(),11.2,219,0.32});
+        flexTdChainMapper.insertInto("d1001").valuesList(list,o->(Object[])o ).execute();
+    }
+    /**
+     * 测试插入并自动建表
+     */
+    @Test
+    public void testInsertAndCreate(){
+        flexTdChainMapper.insertInto("d2001").using("meters").tags(2,"abc").values(new Date(),10.2,219,0.32).execute();
     }
 }
